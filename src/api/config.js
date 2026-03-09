b67bbcaf7ba6bd309b6ed3f3e510fc1cb9f3e7c2
Deepak Bhat <135301126+deepakbhat18@users.noreply.github.com>
2026-03-09 18:39:49 +0530

    Update config.js
    Removed

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
-export const RAZORPAY_KEY = import.meta.env.VITE_RAZORPAY_KEY || "rzp_test_RvqjGKh8Ekvg4c";
+export const RAZORPAY_KEY = import.meta.env.VITE_RAZORPAY_KEY;

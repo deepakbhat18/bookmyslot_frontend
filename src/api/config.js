c2d4f3b34313d91895b8034538f57d2bd1008074
Deepakbhat-18 <deepakravindrabhat@gmail.com>
2026-03-10 17:17:37 +0530

    added backend url

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,4 @@
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+export const API_BASE_URL = "https://bookmyslot-eeh4.onrender.com";
 export const RAZORPAY_KEY = import.meta.env.VITE_RAZORPAY_KEY;
